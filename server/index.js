b321efd36e73beaf53978015bcab7299be681955
Loc Nguyen <tanlocit20062000@gmail.com>
2025-08-15 22:02:25 +0700

    done lesson 3

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import { v4 as uuidv4 } from "uuid";
 import fs from "fs";
-
-// import { customers, orders, products } from "./data.js";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 app.use(express.json());
@@ -13,139 +11,146 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const dataPath = path.join(__dirname, "data.json");
 
-// Hàm đọc file data.json
 const readData = () => {
   const data = fs.readFileSync(dataPath, "utf8");
   return JSON.parse(data);
 };
 
-// Hàm ghi file data.json
 const writeData = (data) => {
   fs.writeFileSync(dataPath, JSON.stringify(data, null, 2), "utf8");
 };
 
-// phương thức get với base API http://localhost:8080/hello
-// 4 phương thúc chính là GET, POST, PUT, DELETE
-app.get("/hello", (req, res) => {
-  res.send({ message: "hello mindx" });
-});
-
-// bài 1 viết api trả về danh sách customers
+// bài 1
 app.get("/customers", (req, res) => {
-  let data = readData();
-  const listCustomer = data.customers;
+  const data = readData();
+  const customers = data?.customers;
   res.send({
-    data: listCustomer.length > 0 ? listCustomer : [],
-    total: listCustomer.length,
+    data: customers?.length > 0 ? customers : [],
+    total: customers?.length,
   });
 });
 
-// viết api get customer detail
+// bài 2
 app.get("/customers/:id", (req, res) => {
   const { id } = req.params;
-  let data = readData();
-  const foundCustomerDetail = data.customers.find((customer) => customer.id === id);
-  if (foundCustomerDetail) {
-    res.send({ data: foundCustomerDetail }); // trong cái response api sẽ trả về { data: ..., message: ..., status: true/ false }
-  } else res.status(404).send({ message: "Customer not found !!!" });
+  const data = readData();
+  const customers = data?.customers;
+  const customerExisted = customers.find((customer) => customer.id === id);
+  if (customerExisted) {
+    res.send({
+      data: customerExisted,
+    });
+  } else {
+    res.send({
+      message: "Customer not found",
+    });
+  }
 });
 
-// viết api lấy danh sách đơn hàng của 1 khách hàng cụ thể endpoint: /customers/:customerId/orders
+// bài 3 /customers/:customerId/orders
 app.get("/customers/:customerId/orders", (req, res) => {
   const { customerId } = req.params;
-  let data = readData();
-  const foundListOrder = data.orders.filter((orders) => orders.customerId === customerId);
-  res.send({ data: foundListOrder });
+  const data = readData();
+  const listOrdersFound = data?.orders.filter(
+    (order) => order.customerId === customerId
+  );
+  res.send({ data: listOrdersFound });
 });
 
+// bài 4
 app.get("/orders/high-value", (req, res) => {
-  let data = readData();
-  const foundOrderHigh = data.orders.filter((order) => order.totalPrice >= 10000000);
-  res.send({ data: foundOrderHigh });
+  const data = readData();
+  const listOrderHighValue = data?.orders.filter(
+    (order) => order.totalPrice >= 10000000
+  );
+  res.send({ data: listOrderHighValue });
 });
 
+// bài 5
 app.get("/products", (req, res) => {
   const { minPrice, maxPrice } = req.query;
   const minPriceValue = minPrice ? Number(minPrice) : null;
   const maxPriceValue = maxPrice ? Number(maxPrice) : null;
-  let data = readData();
+  const data = readData();
   let listProduct = [];
   if (!!minPrice && !!maxPrice) {
-    listProduct = data.products.filter(
+    listProduct = data?.products.filter(
       (pro) => pro.price >= minPriceValue && pro.price <= maxPriceValue
     );
   } else if (!!minPriceValue) {
-    listProduct = data.products.filter((pro) => pro.price >= minPriceValue);
+    listProduct = data?.products.filter((pro) => pro.price >= minPriceValue);
   } else if (!!maxPriceValue) {
-    listProduct = data.products.filter((pro) => pro.price <= maxPriceValue);
+    listProduct = data?.products.filter((pro) => pro.price <= maxPriceValue);
   }
   res.send({ data: listProduct });
 });
 
-// viết api thêm customer mới
+// bài 6 thêm mới khách hàng
 app.post("/customers", (req, res) => {
   const { name, email, age } = req.body;
+  if (!name || !email || !age)
+    res
+      .status(404)
+      .send({ message: "Vui lòng nhập đủ thông tin email, name, age" });
 
-  if (!name || !email || !age) return res.status(400).send({ message: "Thiếu thông tin name, email hoặc age" });
   let data = readData();
-
-  // Kiểm tra email trùng
-  const emailExists = data.customers.some(
+  const emailExisted = data?.customers.some(
     (customer) => customer.email.toLowerCase() === email.toLowerCase()
   );
-  if (emailExists) return res.status(400).send({ message: "Email đã tồn tại" });
-
-  let newId;
-  do {
-    newId = uuidv4();
-  } while (data.customers.some((c) => c.id === newId));
-  const newCustomer = { id: newId, name, email, age };
-  data.customers.push(newCustomer);
-  writeData(data);
-  res.status(201).json(newCustomer);
+  if (emailExisted) {
+    res.status(400).send({ message: "Email đã tồn tại." });
+  } else {
+    const newCustomer = { id: uuidv4(), name, email, age };
+    data.customers.push(newCustomer);
+    writeData(data);
+    res.status(201).send({ data: newCustomer });
+  }
 });
 
+// bài 7 tạo mới 1 đơn hàng
 app.post("/orders", (req, res) => {
   const { customerId, productId, quantity } = req.body;
   const orderId = uuidv4();
-  if (!customerId || !productId || !quantity) {
-    return res.status(400).send({ message: "Thiếu thông tin customerId, productId hoặc quantity" });
-  }
+  if (!customerId || !productId || !quantity)
+    res.status(404).send({
+      message: "Vui lòng nhập đủ thông tin customerId, productId, quantity",
+    });
 
   let data = readData();
+  const customerExisted = data?.customers.find((cus) => cus.id === customerId);
+  if (!customerExisted)
+    res.status(404).send({ message: "Customer not found. " });
 
-  const customer = data.customers.find((c) => c.id === customerId);
-  if (!customer) return res.status(404).send({ message: "Customer không tồn tại" });
+  const productExisted = data?.products.find((pro) => pro.id === productId);
+  if (!productExisted) res.status(404).send({ message: "Product not found. " });
 
-  const product = data.products.find((p) => p.id === productId);
-  if (!product) return res.status(404).send({ message: "Product không tồn tại" });
+  const orderIdExisted = data?.orders.some((ord) => ord.id === orderId);
+  if (!orderIdExisted) {
+    const totalPrice = productExisted.price * quantity;
+    // tạo đơn hàng mới
+    const newOrder = {
+      id: orderId,
+      customerId,
+      productId,
+      quantity,
+      totalPrice,
+    };
 
-  // Kiểm tra số lượng hợp lệ
-  if (quantity <= 0 || quantity > product.quantity) {
-    return res.status(400).send({ message: "Số lượng không hợp lệ hoặc vượt quá tồn kho" });
+    if (quantity <= 0 || quantity > productExisted.quantity) {
+      res
+        .status(400)
+        .send({ message: "Số lượng không hợp lệ hoặc vượt quá tồn kho." });
+    } else {
+      data.orders.push(newOrder);
+      productExisted.quantity -= quantity;
+      writeData(data);
+      res.status(201).send({ data: newOrder });
+    }
+  } else {
+    res.status(400).send({ message: "Order đã tồn tại." });
   }
-
-  // Kiểm tra orderId không trùng
-  const orderExists = data.orders.some((o) => o.id === orderId);
-  if (orderExists) {
-    return res.status(400).send({ error: "OrderId đã tồn tại" });
-  }
-
-  const totalPrice = product.price * quantity;
-
-  // Tạo đơn hàng mới
-  const newOrder = {
-    id: orderId,
-    customerId,
-    productId,
-    quantity,
-    totalPrice,
-  };
-  data.orders.push(newOrder);
-  product.quantity -= quantity;
-  writeData(data);
-  res.status(201).send(newOrder);
 });
+
 
 app.put("/orders/:orderId", (req, res) => {
   const { orderId } = req.params;
